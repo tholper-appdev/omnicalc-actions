f6424a7ce40b4bb321d4c23044e53a18457de5e6
Tim Holper <tholper@uchicago.edu>
2018-10-13 21:47:37 +0000

    version_sat_1

--- app/controllers/word_count_controller.rb
+++ app/controllers/word_count_controller.rb
@@ -9,14 +9,14 @@ class WordCountController < ApplicationController
     # The special word the user input is in the string @special_word.
     # ================================================================================
 
-    @word_count = "Replace this string with your answer"
+    @word_count = @text.split(" ").length.to_s
+    
+    @character_count_with_spaces = @text.size.to_s
 
-    @character_count_with_spaces = "Replace this string with your answer"
+    @character_count_without_spaces = @text.gsub(/\s+/, "").size.to_s
 
-    @character_count_without_spaces = "Replace this string with your answer"
-
-    @occurrences = "Replace this string with your answer"
-
+    @occurrences = @text.scan(/(?=#{@special_word})/).count
+  
     # ================================================================================
     # Your code goes above.
     # ================================================================================
