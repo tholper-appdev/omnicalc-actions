3b03b116f6046787acbdc9b231cf6e6a96e90051
Tim Holper <tholper@uchicago.edu>
2018-10-15 19:26:47 +0000

    version_mon_2

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -11,16 +11,23 @@ class MeteorologistController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_street_address + "&key=AIzaSyA5qwIlcKjijP_Ptmv46mk4cCjuWhSzS78"
+    parsed_data = JSON.parse(open(url).read)
+    @lat = parsed_data.dig("results", 0, "geometry", "location", "lat").to_s
+    @lng = parsed_data.dig("results", 0, "geometry", "location", "lng").to_s 
+    
+    url = "https://api.darksky.net/forecast/b9a02ebb30de92e038fd4378270b0dfc/" + @lat + "," + @lng
+    parsed_data = JSON.parse(open(url).read)
+    
+    @current_temperature = parsed_data.dig("currently","temperature")
+    
+    @current_summary = parsed_data.dig("currently","summary")
 
-    @current_temperature = "Replace this string with your answer"
+    @summary_of_next_sixty_minutes = parsed_data.dig("minutely","summary")
 
-    @current_summary = "Replace this string with your answer"
+    @summary_of_next_several_hours = parsed_data.dig("hourly","summary")
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer"
-
-    @summary_of_next_several_hours = "Replace this string with your answer"
-
-    @summary_of_next_several_days = "Replace this string with your answer"
+    @summary_of_next_several_days = parsed_data.dig("daily","summary")
 
     render("meteorologist_templates/street_to_weather.html.erb")
   end
