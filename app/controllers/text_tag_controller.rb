0c4874d662e24bd25b82ac1d31c7f3d9c7c3132e
Tim Holper <tholper@uchicago.edu>
2018-10-16 15:04:52 +0000

    version_tues_1

--- app/controllers/text_tag_controller.rb
+++ app/controllers/text_tag_controller.rb
@@ -1,16 +1,11 @@
+require 'algorithmia'
+
 class TextTagController < ApplicationController
   def text_tag
     @text = params.fetch("text")
-
-    # ================================================================================
-    # Your code goes below.
-    # ================================================================================
-
-    @tags = "Replace this string with your answer"
-
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
+    client = Algorithmia.client('simtssusGxD8cl6mWJ0YkIivFb81')
+    algo = client.algo('nlp/AutoTag/1.0.1')
+    @tags = algo.pipe(@text).result
 
     render("text_tag_templates/text_tag.html.erb")
   end
